381c1df2ed2df547aa94fb4272a6ca5a5e8ad4ac
JoshParkr <joshua.parker2@g.austincc.edu>
2024-09-09 18:33:47 +0000

    Homework 1 non-tested

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,23 @@
 ﻿//write include statements
-
+#include "iostream"
+#include "data_types.h"
 
 //write namespace using statement for cout
 
 
 int main()
 {
-	
+	int num;
+	std::cin >> num;
+
+	int result = multiply_numbers(num);
+
+	std::cout << result;
+
+	int num1 = 4;
+
+	result = multiply_numbers(num1);
+	std::cout << result;
 
 	return 0;
 }
