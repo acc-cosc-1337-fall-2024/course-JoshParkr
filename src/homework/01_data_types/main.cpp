2e520cc1b61fa35bf6a62574c88b4f3d27a3ce26
JoshParkr <joshua.parker2@g.austincc.edu>
2024-09-09 23:47:43 +0000

    Updated namespace Hw

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,9 +1,9 @@
 ﻿//write include statements
-#include "iostream"
+#include<iostream>
 #include "data_types.h"
 
 //write namespace using statement for cout
-
+using std::cout;
 
 int main()
 {
@@ -12,12 +12,12 @@ int main()
 
 	int result = multiply_numbers(num);
 
-	std::cout << result;
+	cout << result;
 
 	int num1 = 4;
 
 	result = multiply_numbers(num1);
-	std::cout << result;
+	cout << result;
 
 	return 0;
 }
